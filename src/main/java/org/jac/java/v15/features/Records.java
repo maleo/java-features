c2645a82cfb7a779ba3a39b8d9a58816f2a749ef
Junling Guo <junling.guo@junlings-mbp.attlocal.net>
2021-01-31 12:27:10 -0800

    v15 Edward Curve Digital Signature Usage Demo

---
+++ src/main/java/org/jac/java/v15/features/Records.java
@@ -0,0 +1,35 @@
+package org.jac.java.v15.features;
+
+/**
+ * A record class is a shallowly immutable, transparent carrier for a fixed set of values, called the record components.
+ * The Java™ language provides concise syntax for declaring record classes, whereby the record components are declared
+ * in the record header. The list of record components declared in the record header form the record descriptor.
+ *
+ */
+public class Records {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1,2);
+        //toString
+        System.out.println("p1.toString(): " + p1);
+
+        //Equality
+        Point p2 = new Point(1, 2);
+        System.out.println("p1 == p2: " + (p1 == p2));
+        System.out.println("p1.equals(p2): " + p1.equals(p2));
+
+        //Invoking self-defined instance method
+        System.out.println(p2.toJson());
+        System.out.println(p1.toJson());
+    }
+}
+
+record Point(int x, int y) {
+    //You can't define instance field in record
+    //int z;
+
+    //You can define instance method in record
+    public String toJson() {
+        return "{x:" + x + ",y:" + y +"}";
+    }
+}
